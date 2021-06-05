b9d52bf25d13e8b6cc858a8d3433588bd9a4f485
Nitish Gupta <imnitish.ng@gmail.com>
2021-06-06 01:07:47 +0530

    [Django][Tests] Fix tests

--- musebackendserver/muse/apps/endpoints/tests.py
+++ musebackendserver/muse/apps/endpoints/tests.py
@@ -8,28 +8,28 @@ class EndpointTests(TestCase):
 
     def test_recommendations_view(self):
         client = APIClient()
-        input_data = {
+        
+        payload = {
             "song": "cheapskate",
             "artist": "oliver tree"
         }
-        recommendations_url = "/api/v1/sp_recommedations"
-        response = client.post(recommendations_url, input_data, format="json")
+        url = "/api/v1/sp_recommedations"
+        response = client.post(url, payload, format="json")
         
-        print(response.data)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data["status"], "OK")
-        self.assertTrue("recommendations" in response.data)
+        self.assertEqual(response.data["query_id"], 1)
+        self.assertEqual(len(response.data["recommendations"]), 50)
+        self.assertEqual(len(response.data["recommendation_ids"]), 50)
 
     def test_lyrics_fetch(self):
         client = APIClient()
         populate_test_db()
 
-        input_data = {
+        payload = {
             "query_id": "1"
         }
-        lyrics_url = "/api/v1/get_lyrics"
-        response = client.post(lyrics_url, input_data, format="json")
+        url = "/api/v1/show_lyrics"
+        response = client.post(url, payload, format="json")
         
-        print(response.data)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data["status"], "OK")
