28d38124e76b77b8b4481558b833319c26989fe6
Nitish Gupta <imnitish.ng@gmail.com>
2021-06-12 08:59:56 +0530

    [CSS] Move tailwind CSS to postprocessing using `craco`
    - Add tailwind.config.js to extend styles
    - Change start, build, test scripts to use `craco`
    - Change CSS imports
    - Removed unnecessary files
    - Update .gitignore

--- muse-frontend/src/App.js
+++ muse-frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import './assets/main.css'
 import Search from './components/search'
 import RecommendationsPanel from './components/resultsPanel'
 import RecommendationsBlock from './components/recommendations'
--- muse-frontend/src/components/recommendations.js
+++ muse-frontend/src/components/recommendations.js
@@ -4,10 +4,13 @@ import { useSelector } from 'react-redux'
 const Track = ({  trackImage, trackNameToShow }) => {
   return (
     <>
-      <li className="py-0.5 cursor-pointer hover:bg-red-50 hover:text-gray-900">
-        <div className="flex flex-row">
+      <li className="py-0.5 hover:bg-red-50 hover:text-gray-900">
+        <div className="flex flex-row h-full w-full">
+          <div className="w-5/100 bg-green-100">
+            ok
+          </div>
           <div className="flex-none">
-            <img src={trackImage} alt={trackNameToShow} className="h-50" width="50"/>
+            <img src={trackImage} alt={trackNameToShow} className="h-50 w-50"/>
           </div>
           <div className="ml-3 truncate">
             {trackNameToShow}
