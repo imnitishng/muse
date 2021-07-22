dc6c3d42d71a16ff49ea95a114f77c659397f5d9
Nitish Gupta <imnitish.ng@gmail.com>
2021-07-22 19:39:30 +0530

    [Spiders] Add config files, script and support env vars for docker
    - Add entrypoint shell script to run spiders inside docker
    - Add config files for starting scrapyd on defined URL
    - Support env variables inside spiders

--- musebackendserver/muse/spiders/lyricraper/initiator.py
+++ musebackendserver/muse/spiders/lyricraper/initiator.py
@@ -1,5 +1,6 @@
 from scrapy.utils.log import configure_logging
 from scrapyd_api import ScrapydAPI
+from scrapyd_client.utils import get_config
 
 from .utils import getSongIDsToScrape1
 
@@ -25,4 +26,6 @@ def crawl(song_ids_arg, recommendation_id_hex):
     return spider_job_id
 
 def get_spider_instance():
-    return ScrapydAPI('http://localhost:6800')
+    DEFAULT_TARGET_URL = 'http://localhost:6800'
+    target_url = get_config('deploy', 'url', fallback=DEFAULT_TARGET_URL).rstrip('/')
+    return ScrapydAPI(target_url)
